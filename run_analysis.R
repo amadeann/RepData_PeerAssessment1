ac1d9a177f36e5062748bcea53d1dd0263b3b73a
amadeann <annissimo@gmail.com>
2015-11-06 08:59:10 +0100

    adding .gitignore

--- run_analysis.R
+++ run_analysis.R
@@ -7,3 +7,21 @@ if(!file.exists(activityFile)) {
 } else {
     message(paste(activityFile, "file already existed."))   
 }
+
+# Load the dataset
+
+activity <- read.csv(activityFile)
+
+# Generate a histogram
+
+activityDaily <- activity %>%
+                    group_by(date) %>% 
+                    summarise(stepsSum = sum(steps, na.rm = TRUE)) %>% 
+                    as.data.frame()
+
+library(ggplot2)
+qplot(activityDaily$stepsSum, geom="histogram") 
+# Histogram with a smaller number of bins
+qplot(activityDaily$stepsSum, geom="histogram", binwidth = 2000) 
+# now add the axis labels
+
