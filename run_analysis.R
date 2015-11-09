846e198f5267bee6a3b360e6248f58711e51ca86
amadeann <annissimo@gmail.com>
2015-11-09 08:21:20 +0100

    modifying file

--- run_analysis.R
+++ run_analysis.R
@@ -14,14 +14,43 @@ activity <- read.csv(activityFile)
 
 # Generate a histogram
 
+library(dplyr)
+
 activityDaily <- activity %>%
                     group_by(date) %>% 
                     summarise(stepsSum = sum(steps, na.rm = TRUE)) %>% 
                     as.data.frame()
 
 library(ggplot2)
-qplot(activityDaily$stepsSum, geom="histogram") 
-# Histogram with a smaller number of bins
-qplot(activityDaily$stepsSum, geom="histogram", binwidth = 2000) 
-# now add the axis labels
+hist1 <- qplot(activityDaily$stepsSum, geom="histogram", xlab = "Daily sum of steps") 
+hist1 <- hist + ggtitle("Histogram of daily sums of steps")
+hist1
 
+# Calculating daily mean and median number of steps
+mean(activityDaily$stepsSum)
+median(activityDaily$stepsSum)
+
+# Average number of steps in 5-minute interval
+
+activityInterval <- activity %>%
+    group_by(interval) %>% 
+    summarise(stepsAvg = mean(steps, na.rm = TRUE)) %>% 
+    as.data.frame()
+
+hist2 <- ggplot(activityInterval, aes(interval, stepsAvg)) + geom_line() 
+hist2 <- hist2 + xlab("5-minute intervals") + ylab("Average number of steps")
+hist2 <- hist2 + ggtitle("Average number of steps per interval")
+hist2
+
+# Interval with the maximum number of steps
+
+intervalMaxAvg <- activityInterval[which.max(activityInterval$stepsAvg),]
+print(intervalMaxAvg, row.names = FALSE)
+
+hist2 <- hist2 + geom_point(data = intervalMaxAvg, color = "red")
+hist2 + annotate("text",
+                 label = paste("max: ", intervalMaxAvg[,1], ", ", round(intervalMaxAvg[,2], digits = 2), sep = ""),
+                 x=intervalMaxAvg[,1] + 25,
+                 y=intervalMaxAvg[,2],
+                 hjust = 0,
+                 size = 4)
